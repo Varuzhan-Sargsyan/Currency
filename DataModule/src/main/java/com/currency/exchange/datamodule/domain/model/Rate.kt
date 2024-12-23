6d222df1cf01580438e7ad12873a468d2888df3d
Varuzhan-Job <varuzhan.sargsyan.job@gmail.com>
2024-12-23 21:39:07 +0400

    - new theme - rates entities, api, database, etc - cleanup - optimized data loading and reloading - refactoring

---
+++ DataModule/src/main/java/com/currency/exchange/datamodule/domain/model/Rate.kt
@@ -0,0 +1,7 @@
+package com.currency.exchange.datamodule.domain.model
+
+data class Rate(
+    val sellCurrency: Currency,
+    val buyCurrency: Currency,
+    val rate: Float,
+)
---
+++ DataModule/src/main/java/com/currency/exchange/datamodule/utils/DateExt.kt
@@ -0,0 +1,20 @@
+package com.currency.exchange.datamodule.utils
+
+import com.currency.exchange.datamodule.utils.Const.dateFormatter
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Date
+import java.util.Locale
+
+object Const {
+    val dateFormatter: ThreadLocal<SimpleDateFormat> = ThreadLocal.withInitial {
+        SimpleDateFormat("yyyy-MM-dd", Locale.US) // Use a fixed locale for consistency
+    }
+
+    fun now() =
+        Calendar.getInstance(Locale.getDefault()).time
+}
+
+fun Date.toCurrencyDateString() =
+    dateFormatter.get()?.format(this) ?: ""
+
